48c8b2b58c7ac965dcbeba4be05d126e88eacca3
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-07-15 15:20:10 +0300

    normalize style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Start from '../pages/Start/Start';
 import Beer from '../pages/Beer/Beer';
+import GlobalStyle from './globalStyled';
 
 function App() {
   return (
-    <Routes>
+  <>
+      <GlobalStyle />
+      <Routes>
           <Route path="/" element={<Start />} />
           <Route path="/beer" element={<Beer />} />
     </Routes>
+  </>
   );
 }
 
